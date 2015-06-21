a2eb60b6dbcc5c344557a7d819661135e09892e5
Yoshua Wuyts <i@yoshuawuyts.com>
2015-06-21 23:49:10 +0200

    test: include breakpoint test

--- test.js
+++ test.js
@@ -27,6 +27,15 @@ test('should get the current viewport', function (t) {
   t.equal(vp(), 'lap')
 })
 
+test('should return different values based on the viewport', function (t) {
+  t.plan(5)
+  t.equal(vp(100), 'wrist')
+  t.equal(vp(700), 'palm')
+  t.equal(vp(1000), 'lap')
+  t.equal(vp(2000), 'desk')
+  t.equal(vp(8000), 'wall')
+})
+
 function setViewport (val) {
   window.innerWidth = val
   document.documentElement.clientWidth = val
